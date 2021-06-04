547ab245a0078fa1dd0e1b8440310f9ca10224da
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-06-04 15:52:33 -0500

    Added pokemon data successfully

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require(`express`);
+const pokemon = require(`./models/pokemon`)
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + `/public`));
+
+
+app.get(`/`, (req, res) => {
+    res.send(pokemon)
+})
+
+app.listen(port)
+
+
+
 
