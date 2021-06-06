5cba136aeacb0f2362dfc93b2c22d06b09c301ad
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-06-06 17:53:14 -0500

    FInished adding edit feature

--- server.js
+++ server.js
@@ -1,54 +1,32 @@
 const express = require(`express`);
 const methodoverride = require('method-override');
 const pokemons = require(`./models/pokemon`)
-const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(methodoverride('_method'));
-app.use(express.json());
 app.use((req, res, next) => {
     next()
 });
+app.use(methodoverride('_method'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + `/public`));
 
-
-// Lists pokemon from model
+// Index page
 app.get(`/index`, (req, res) => {
+
     res.render(`index.ejs`,
         { pokemons })
 })
-// Add news poke to index
-app.post(`/index`, (req, res) => {
 
-    pokemons.push(req.body)
-
-})
-// Routes to create page
+// create new page
 app.get(`/index/create`, (req, res) => {
-    res.render(`create.ejs`)
-})
-
-// render edit page
-app.get(`/index/:id/edit`, (req, res) => {
-    res.render(`edit.ejs`, {
-        pokemon: pokemons[req.params.id], index: req.params.id
+    res.render(`create.ejs`, {
+        pokemon: pokemons[0]
     })
 })
 
-// edit route
-app.put(`/index/:id`, (req, res) => {
-    pokemons[req.params.id] = req.body;
-    res.redirect(`/index`)
-})
-
-app.delete(`/index/:id`, (req, res) => {
-    pokemons.splice(req.params.id, 1)
-    res.redirect(`/index`)
-})
-
-// Individual pokemon
+//Show pokemon
 app.get(`/index/:id`, (req, res) => {
     res.render(`show.ejs`,
         {
@@ -56,7 +34,34 @@ app.get(`/index/:id`, (req, res) => {
         })
 })
 
+// render edit page
+app.get(`/index/:id/edit`, (req, res) => {
+    res.render(`edit.ejs`, {
+        pokemon: pokemons[req.params.id],
+        index: req.params.id
+    })
+})
 
+// Add new pokemon to index
+app.post(`/index`, (req, res) => {
+    pokemons.push(req.body)
+    res.redirect(`/index`)
+})
+
+// delete pokemon
+app.delete(`/index/:id`, (req, res) => {
+    pokemons.splice(req.params.id, 1)
+    res.redirect(`/index`)
+})
+
+// update pokemon
+app.put(`/index/:id`, (req, res) => {
+
+
+    pokemons[req.params.id] = req.body
+
+    res.redirect(`/index`)
+})
 
 app.listen(port)
 
